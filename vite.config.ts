a003a6943159a29a6a79c65192071d9072b3cccc
manas jha <manasjha384@gmail.com>
2026-07-13 16:24:25 +0530

    modify the vite file

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Weather-app/",
+  base: "/weather-app/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
