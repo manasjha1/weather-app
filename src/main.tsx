8dd7287a7c972b5dcb9d684fc3af8d3d13dcaa57
manas jha <manasjha384@gmail.com>
2026-07-13 16:33:58 +0530

    change the routing error

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/weather-app">
     <StrictMode>
       <App />
     </StrictMode>,
